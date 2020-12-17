3201ed7c333f24900ac7f7f6ea3f1be7cedf1a59
Tony Theodoropoulos <theodot@outlook.com>
2020-12-17 12:34:08 -0500

    Add project files.

---
+++ ArrayExamples/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace ArrayExamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.q3Output = new System.Windows.Forms.Label();
+            this.q3Label = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.q2Output = new System.Windows.Forms.Label();
+            this.q2Label = new System.Windows.Forms.Label();
+            this.q1Output = new System.Windows.Forms.Label();
+            this.q1Label = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // q3Output
+            // 
+            this.q3Output.BackColor = System.Drawing.Color.White;
+            this.q3Output.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q3Output.ForeColor = System.Drawing.Color.Black;
+            this.q3Output.Location = new System.Drawing.Point(29, 238);
+            this.q3Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q3Output.Name = "q3Output";
+            this.q3Output.Size = new System.Drawing.Size(156, 20);
+            this.q3Output.TabIndex = 27;
+            this.q3Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // q3Label
+            // 
+            this.q3Label.AutoSize = true;
+            this.q3Label.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q3Label.ForeColor = System.Drawing.Color.White;
+            this.q3Label.Location = new System.Drawing.Point(26, 222);
+            this.q3Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q3Label.Name = "q3Label";
+            this.q3Label.Size = new System.Drawing.Size(63, 13);
+            this.q3Label.TabIndex = 26;
+            this.q3Label.Text = "Question 3";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-1, -1);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(219, 52);
+            this.titleLabel.TabIndex = 24;
+            this.titleLabel.Text = "Array Examples";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // q2Output
+            // 
+            this.q2Output.BackColor = System.Drawing.Color.White;
+            this.q2Output.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q2Output.ForeColor = System.Drawing.Color.Black;
+            this.q2Output.Location = new System.Drawing.Point(29, 189);
+            this.q2Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q2Output.Name = "q2Output";
+            this.q2Output.Size = new System.Drawing.Size(156, 20);
+            this.q2Output.TabIndex = 23;
+            this.q2Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // q2Label
+            // 
+            this.q2Label.AutoSize = true;
+            this.q2Label.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q2Label.ForeColor = System.Drawing.Color.White;
+            this.q2Label.Location = new System.Drawing.Point(26, 173);
+            this.q2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q2Label.Name = "q2Label";
+            this.q2Label.Size = new System.Drawing.Size(63, 13);
+            this.q2Label.TabIndex = 22;
+            this.q2Label.Text = "Question 2";
+            // 
+            // q1Output
+            // 
+            this.q1Output.BackColor = System.Drawing.Color.White;
+            this.q1Output.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Output.ForeColor = System.Drawing.Color.Black;
+            this.q1Output.Location = new System.Drawing.Point(29, 143);
+            this.q1Output.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q1Output.Name = "q1Output";
+            this.q1Output.Size = new System.Drawing.Size(156, 20);
+            this.q1Output.TabIndex = 21;
+            this.q1Output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // q1Label
+            // 
+            this.q1Label.AutoSize = true;
+            this.q1Label.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.q1Label.ForeColor = System.Drawing.Color.White;
+            this.q1Label.Location = new System.Drawing.Point(26, 126);
+            this.q1Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.q1Label.Name = "q1Label";
+            this.q1Label.Size = new System.Drawing.Size(63, 13);
+            this.q1Label.TabIndex = 20;
+            this.q1Label.Text = "Question 1";
+            // 
+            // runButton
+            // 
+            this.runButton.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.runButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.runButton.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runButton.ForeColor = System.Drawing.Color.White;
+            this.runButton.Location = new System.Drawing.Point(29, 79);
+            this.runButton.Margin = new System.Windows.Forms.Padding(2);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(156, 25);
+            this.runButton.TabIndex = 19;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = false;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(216, 301);
+            this.Controls.Add(this.q3Output);
+            this.Controls.Add(this.q3Label);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.q2Output);
+            this.Controls.Add(this.q2Label);
+            this.Controls.Add(this.q1Output);
+            this.Controls.Add(this.q1Label);
+            this.Controls.Add(this.runButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arrays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label q3Output;
+        private System.Windows.Forms.Label q3Label;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label q2Output;
+        private System.Windows.Forms.Label q2Label;
+        private System.Windows.Forms.Label q1Output;
+        private System.Windows.Forms.Label q1Label;
+        private System.Windows.Forms.Button runButton;
+    }
+}
+
---
+++ ArrayExamples/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayExamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // 1. Add 1 to each value in given array and display to screen
+            //
+            // Sample Output:
+            // 2 3 4 5 6 
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+            }
+
+
+            // 2. Sum all the values in the given array and display result
+            //
+            // Sample Output:
+            // The sum is 20
+
+
+
+            // 3. Perform the following on the given array, then display
+            //    the updated array values.
+            //    
+            //    - add 5 to the element at index 1
+            //    - multiple the element at index 2 by element 
+            //      at index 4, and place result back at index 2
+            //    - subtract the element at index 1 from element 
+            //      at index 0 and place result in at index 0
+            //    - swap the elements at index 3 and index 4
+            //
+            // Sample Output
+            // -6 8 24 6 5
+
+
+        }
+    }
+}
