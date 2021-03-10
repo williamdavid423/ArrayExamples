02a6066ac5c35bf2294d6ef129be9c7cd08d15eb
Tony Theodoropoulos <theodot@outlook.com>
2021-03-10 14:56:26 -0500

    updated with /// comments

--- ArrayExamples/Form1.cs
+++ ArrayExamples/Form1.cs
@@ -21,10 +21,11 @@ namespace ArrayExamples
         {
             int[] numbers = new int[] { 1, 2, 3, 4, 5 };
 
-            // 1. Add 1 to each value in given array and display to screen
-            //
-            // Sample Output:
-            // 2 3 4 5 6 
+            /// 1. Add 1 to each value in given array and display
+            /// to the screen
+            /// 
+            /// Sample Output:
+            /// 2 3 4 5 6 
 
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -32,25 +33,26 @@ namespace ArrayExamples
             }
 
 
-            // 2. Sum all the values in the given array and display result
-            //
-            // Sample Output:
-            // The sum is 20
+            /// 2. Sum all the values in the given array and 
+            /// display result
+            /// 
+            /// Sample Output:
+            /// The sum is 20
 
 
 
-            // 3. Perform the following on the given array, then display
-            //    the updated array values.
-            //    
-            //    - add 5 to the element at index 1
-            //    - multiple the element at index 2 by element 
-            //      at index 4, and place result back at index 2
-            //    - subtract the element at index 1 from element 
-            //      at index 0 and place result in at index 0
-            //    - swap the elements at index 3 and index 4
-            //
-            // Sample Output
-            // -6 8 24 6 5
+            /// 3. Perform the following on the given array, then 
+            /// display the updated array values.
+            ///
+            /// - add 5 to the element at index 1
+            /// - multiple the element at index 2 by element 
+            /// at index 4, and place result back at index 2
+            /// - subtract the element at index 1 from element 
+            /// at index 0 and place result in at index 0
+            /// - swap the elements at index 3 and index 4
+            /// 
+            /// Sample Output
+            /// -6 8 24 6 5
 
 
         }
